fcfab7f6066519673e6d0e54eade806ede0d9800
shameer125 <alishameer251@gmail.com>
2026-05-22 23:51:08 -0700

    update ourwork component

--- src/components/OurWork.jsx
+++ src/components/OurWork.jsx
@@ -1,34 +1,35 @@
-import React from 'react'
-import Title from './Title'
-import assets from '../assets/assets';
-import {motion} from "motion/react";
+import React from "react";
+import Title from "./Title";
+import assets from "../assets/assets";
+import { motion } from "motion/react";
 
 const OurWork = () => {
-
-    const workData = [
-        {
-            title: 'Mobile app marketing',
-            description: 'We turn bold ideas into powerful digital solutions that connect, engage...',
-            image: assets.work_mobile_app 
-        },
-        {
-            title: 'dashboard mangement',
-            description: 'We help you execute your plan and deliver results.',
-            image: assets.work_dashboard_management
-        },
-        {
-            title: 'fitness app promotion',
-            description: 'We help you create a marketing strategy that drives results.',
-            image: assets.work_fitness_app
-        }
-    ]
+  const workData = [
+    {
+      title: "Mobile app marketing",
+      description:
+        "We turn bold ideas into powerful digital solutions that connect, engage...",
+      image: assets.work_mobile_app,
+    },
+    {
+      title: "dashboard mangement",
+      description: "We help you execute your plan and deliver results.",
+      image: assets.work_dashboard_management,
+    },
+    {
+      title: "fitness app promotion",
+      description:
+        "We help you create a marketing strategy that drives results.",
+      image: assets.work_fitness_app,
+    },
+  ];
 
   return (
     <motion.div
-      initial='hidden'
-      whileInView='visible'
+      initial="hidden"
+      whileInView="visible"
       viewport={{ once: true }}
-      transition={{staggerChildren: 0.2}}
+      transition={{ staggerChildren: 0.2 }}
       id="our-work"
       className="flex flex-col items-center gap-7 px-4 sm:px-12
     lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white"
@@ -37,28 +38,30 @@ const OurWork = () => {
         title="Our latest work"
         desc="From strategy to exectution, we craft digital solutions 
           that move your business forwrad."
-          />
-          
-      <div className='grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full 
-          max-w-5xl'>
-              {
-                  workData.map((work, index) => (
-                    <motion.div
-                      initial={{ opacity: 0, y: 30 }}
-                      whileInView={{ opacity: 1, y: 0 }}
-                      transition={{ duration: 0.5, delay: index * 0.2 }}
-                      viewport={{once: true}}
-                      key={index} className='hover:scale-102 duration-500 transition-all
-                      cursor-pointer'>
-                          <img src={work.image} alt="" className='w-full rounded-r-xl' />
-                          <h3 className='mt-3 mb-2 text-lg font-semibold'>{work.title}</h3>
-                          <p className='text-sm opacity-60 w-5/6'>{work.description }</p>
-                      </motion.div>
-                  ))
-              }
-          </div>
+      />
+
+      <div
+        className="grid sm:grid-cols-2 lg:grid-cols-3 gap-6 w-full 
+          max-w-5xl"
+      >
+        {workData.map((work, index) => (
+          <motion.div
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: index * 0.2 }}
+            viewport={{ once: true }}
+            key={index}
+            className="hover:scale-102 duration-500 transition-all
+                      cursor-pointer"
+          >
+            <img src={work.image} alt="" className="w-full rounded-r-xl" />
+            <h3 className="mt-3 mb-2 text-lg font-semibold">{work.title}</h3>
+            <p className="text-sm opacity-60 w-5/6">{work.description}</p>
+          </motion.div>
+        ))}
+      </div>
     </motion.div>
   );
-}
+};
 
-export default OurWork
+export default OurWork;
