3d1065f5aea0b308c7214f01bbb6a635235f7bd5
shameer125 <alishameer251@gmail.com>
2026-05-22 23:48:46 -0700

    update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = ({ theme, setTheme }) => {
           animate={{ opacity: 1, y: 0 }}
           transition={{duration: 0.6, ease: 'easeInOut'}}
           className='flex justify-between items-center px-4 sm:px-12 lg:24px xl:px-40
-    py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
+          py-4 sticky top-0 z-20 backdrop-blur-xl font-medium bg-white/50 dark:bg-gray-900/70'>
           
           <img src={theme === 'dark' ? assets.logo_dark : assets.logo} alt=""
               className='w-32 sm:40 ' />
@@ -45,6 +45,7 @@ const Navbar = ({ theme, setTheme }) => {
                   Connect <img src={assets.arrow_icon} width={14} alt="" />
               </a>
           </div>
+          
     </motion.div>
   )
 }
